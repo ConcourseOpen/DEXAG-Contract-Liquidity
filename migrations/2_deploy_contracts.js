21bb0fc0e82edaadb45c115be486427e94c8552c
nedodn <odennate@gmail.com>
2019-09-22 17:13:10 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var BuyAndSend = artifacts.require("./BuyAndSend.sol");
 
-var proxy = '0xD3BeD3A8E3e6b24b740EAD108bA776e0Ad298588';
+var proxy = '';
 
 module.exports = function(deployer) {
   deployer.deploy(BuyAndSend, proxy);
